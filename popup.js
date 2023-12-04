8f4f3c8a44b3ecb257f9531b05ef44635081ee19
NGetsee <123643158+NGetsee@users.noreply.github.com>
2023-12-04 14:06:26 +0000

    ee

--- popup.js
+++ popup.js
@@ -1,17 +1,21 @@
+// popup.js
+
 // Function to display a pop-up message
 function displayPopup(message) {
-    // Create the pop-up element
     const popup = document.createElement('div');
     popup.className = 'popup';
     popup.innerHTML = `<p>${message}</p>`;
-
-    // Add the pop-up to the document body
     document.body.appendChild(popup);
+
+    // Close the pop-up when clicked
+    popup.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
 
     // Automatically close the pop-up after a few seconds (adjust the delay as needed)
     setTimeout(() => {
-        document.body.removeChild(popup);
-    }, 10000); // 10 seconds
+        popup.style.display = 'none';
+    }, 5000); // 5 seconds
 }
 
 // Check if a query parameter named "status" exists in the URL
